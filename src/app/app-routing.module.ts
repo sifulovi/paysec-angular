f13c6ce837e759db7021ad49940d60e637c4dd69
nestedbrains <isiful.ovi@gmail.com>
2020-11-20 02:07:00 +0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,19 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {AboutComponent} from './about/about.component';
+import {AgentComponent} from './agent/agent.component';
+import {JoinUsComponent} from './join-us/join-us.component';
+import {ContactComponent} from './contact/contact.component';
+import {PaynowComponent} from './paynow/paynow.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'registration', component: JoinUsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'agent', component: AgentComponent},
+    {path: 'pay', component: PaynowComponent},
+    {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
