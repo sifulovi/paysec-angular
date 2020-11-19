5f7de1bfb34e5143749fc81e88ae78bc27990984
nestedbrains <isiful.ovi@gmail.com>
2020-11-19 23:02:38 +0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {Routes, RouterModule} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
